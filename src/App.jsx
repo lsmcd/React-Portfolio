6f2af58ae1c03f582b5c1c858ee61e67102461d6
Logan McDaniel <lsmcd@pm.me>
2024-05-25 12:21:20 -0600

    add base navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Navbar from './components/Navbar.jsx'
 
 function App() {
   return (
     <>
-      <h1>Empty website</h1>
+      <Navbar />
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+export default function Navbar() {
+    return <>
+        <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Logan McDaniel</a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link">About Me</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">Portfolio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">Resume</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">Contact</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+        </nav>
+    </>
+}
