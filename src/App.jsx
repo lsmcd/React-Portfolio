132bf9fb8b6e95d51c3b80aa50633f65a91faa82
Logan McDaniel <lsmcd@pm.me>
2024-05-25 11:13:11 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <>
+      <h1>Empty website</h1>
+    </>
+  )
+}
+
+export default App
