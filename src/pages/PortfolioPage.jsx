b6bcd163a1ab457572f1ba7de4871ac991c4c204
Logan McDaniel <lsmcd@pm.me>
2024-05-25 19:49:32 -0600

    add portfolio to aboutme redirect for new visitors

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,5 +1,18 @@
+import {useEffect, useState} from "react";
+import { redirect } from "react-router-dom";
+
 export default function PortfolioPage() {
+    
+    useEffect(() => {
+        if(!localStorage.getItem("hasVisitedPortfolio")){
+            localStorage.setItem("hasVisitedPortfolio", "true");
+
+            document.location.pathname = "/aboutme"
+        }
+    });
+
     return <div id="portfoliopage">
         <h1>PortfolioPage</h1>
     </div>
+
 }
