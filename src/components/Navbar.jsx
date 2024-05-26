d2eac81beef131b3361cd4fcf1a8e3bbb9a9fa83
Logan McDaniel <lsmcd@pm.me>
2024-05-25 18:05:00 -0600

    add navbar button disabling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,35 @@
 import { Outlet, Link } from "react-router-dom";
+import { useState, useEffect, useParams } from "react";
 
 export default function Navbar() {
+
+    const [currentPage, setCurrentPage] = useState("")
+
+    function handlePageChange() {
+        let page = document.location.toString().split("/").pop();
+        setCurrentPage(page);
+    }
+    
+    useEffect(() => {
+        handlePageChange()
+    }, []);
+
     return <header>
-        <nav class="navbar navbar-expand-lg bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand">Logan McDaniel</a>
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <Link class="nav-link" to={"aboutme"}>About Me</Link>
+        <nav className="navbar navbar-expand-lg bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand">Logan McDaniel</a>
+                <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li onClick={handlePageChange} className="nav-item">
+                        <Link  className={currentPage === "aboutme" ? "nav-link disabled": "nav-link"} to={"aboutme"}>About Me</Link>
                     </li>
-                    <li class="nav-item">
-                        <Link class="nav-link" to={"portfolio"}>Portfolio</Link>
+                    <li onClick={handlePageChange} className="nav-item">
+                        <Link className={currentPage === "portfolio" ? "nav-link disabled": "nav-link"} to={"portfolio"}>Portfolio</Link>
                     </li>
-                    <li class="nav-item">
-                        <Link class="nav-link" to={"contact"}>Contact</Link>
+                    <li onClick={handlePageChange} className="nav-item">
+                        <Link className={currentPage === "contact" ? "nav-link disabled": "nav-link"} to={"contact"}>Contact</Link>
                     </li>
-                    <li class="nav-item">
-                        <Link class="nav-link" to={"resume"}>Resume</Link>
+                    <li onClick={handlePageChange} className="nav-item">
+                        <Link className={currentPage === "resume" ? "nav-link disabled": "nav-link"} to={"resume"}>Resume</Link>
                     </li>
                 </ul>
             </div>
